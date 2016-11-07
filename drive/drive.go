3524821f3859e1373118e68b73a77fd8138947e1
Andrew Chambers <andrewchamberss@gmail.com>
2016-11-07 21:50:41 +1300

    add more tested helper functions for drive

--- drive/drive.go
+++ drive/drive.go
@@ -2,7 +2,9 @@ package drive
 
 import (
 	"errors"
+	"fmt"
 	"github.com/boltdb/bolt"
+	"strconv"
 	"time"
 )
 
@@ -27,7 +29,7 @@ func Open(dbPath string) (*Drive, error) {
 	defer db.Close()
 
 	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte(MetaDataBucketName))
+		metaDataBucket, err := tx.CreateBucketIfNotExists([]byte(MetaDataBucketName))
 		if err != nil {
 			return err
 		}
@@ -35,6 +37,21 @@ func Open(dbPath string) (*Drive, error) {
 		if err != nil {
 			return err
 		}
+
+		if string(metaDataBucket.Get([]byte("gcgeneration"))) == "" {
+			err = metaDataBucket.Put([]byte("gcgeneration"), []byte("0"))
+			if err != nil {
+				return err
+			}
+		}
+
+		if string(metaDataBucket.Get([]byte("rootversion"))) == "" {
+			err = metaDataBucket.Put([]byte("rootversion"), []byte("0"))
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
 	})
 
@@ -82,19 +99,143 @@ func (d *Drive) Attach(keyId string) (bool, error) {
 }
 
 func (d *Drive) GetGCGeneration() (int64, error) {
-	return 0, errors.New("unimplemented")
+	db, err := openBoltDB(d.dbPath)
+	if err != nil {
+		return -1, err
+	}
+	defer db.Close()
+
+	var gcGenerationString string
+
+	err = db.View(func(tx *bolt.Tx) error {
+		metaDataBucket := tx.Bucket([]byte(MetaDataBucketName))
+		gcGenerationString = string(metaDataBucket.Get([]byte("gcgeneration")))
+		return nil
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	gcGeneration, err := strconv.ParseInt(gcGenerationString, 10, 64)
+	if err != nil {
+		return -1, err
+	}
+
+	return gcGeneration, nil
 }
 
-func (d *Drive) StartGC() error {
-	return errors.New("unimplemented")
+func (d *Drive) StartGC() (int64, error) {
+	db, err := openBoltDB(d.dbPath)
+	if err != nil {
+		return -1, err
+	}
+	defer db.Close()
+
+	var gcGeneration int64
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		metaDataBucket := tx.Bucket([]byte(MetaDataBucketName))
+		gcGeneration, err = strconv.ParseInt(string(metaDataBucket.Get([]byte("gcgeneration"))), 10, 64)
+		if err != nil {
+			return err
+		}
+
+		gcGeneration += 1
+		err = metaDataBucket.Put([]byte("gcgeneration"), []byte(fmt.Sprintf("%d", gcGeneration)))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	return gcGeneration, nil
 }
 
 func (d *Drive) StopGC() error {
-	return errors.New("unimplemented")
+	_, err := d.StartGC()
+	return err
 }
 
 func (d *Drive) CasRoot(root string, newVersion, gcGeneration int64) (bool, error) {
-	return false, errors.New("unimplemented")
+	db, err := openBoltDB(d.dbPath)
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	var ok bool
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		metaDataBucket := tx.Bucket([]byte(MetaDataBucketName))
+		rootVersion, err := strconv.ParseInt(string(metaDataBucket.Get([]byte("rootversion"))), 10, 64)
+		if err != nil {
+			return err
+		}
+
+		rootVersion += 1
+		if rootVersion != newVersion {
+			return nil
+		}
+
+		curGCGeneration, err := strconv.ParseInt(string(metaDataBucket.Get([]byte("rootversion"))), 10, 64)
+		if err != nil {
+			return err
+		}
+
+		if curGCGeneration != gcGeneration {
+			return nil
+		}
+
+		err = metaDataBucket.Put([]byte("rootversion"), []byte(fmt.Sprintf("%d", newVersion)))
+		if err != nil {
+			return err
+		}
+		err = metaDataBucket.Put([]byte("rootval"), []byte(root))
+		if err != nil {
+			return err
+		}
+
+		ok = true
+		return nil
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+func (d *Drive) GetRoot() (string, int64, error) {
+	db, err := openBoltDB(d.dbPath)
+	if err != nil {
+		return "", -1, err
+	}
+	defer db.Close()
+
+	var root string
+	var rootVersion int64
+
+	err = db.View(func(tx *bolt.Tx) error {
+		metaDataBucket := tx.Bucket([]byte(MetaDataBucketName))
+		root = string(metaDataBucket.Get([]byte("rootval")))
+		rootVersion, err = strconv.ParseInt(string(metaDataBucket.Get([]byte("rootversion"))), 10, 64)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return "", -1, err
+	}
+
+	return root, rootVersion, nil
 }
 
 func (d *Drive) AddPack(name string, gcGeneration int64) (bool, error) {
--- drive/drive_test.go
+++ drive/drive_test.go
@@ -43,3 +43,115 @@ func TestAttach(t *testing.T) {
 		t.Fatal("expected ok attach")
 	}
 }
+
+func TestGCGeneration(t *testing.T) {
+	testDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testDir)
+	drive, err := Open(filepath.Join(testDir, "drive.db"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer drive.Close()
+
+	gen, err := drive.GetGCGeneration()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if gen != 0 {
+		t.Fatal("unexpected gcGeneration")
+	}
+
+	gen, err = drive.StartGC()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if gen != 1 {
+		t.Fatal("unexpected gcGeneration")
+	}
+
+	err = drive.StopGC()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gen, err = drive.GetGCGeneration()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if gen != 2 {
+		t.Fatal("unexpected gcGeneration")
+	}
+}
+
+func TestCasRoot(t *testing.T) {
+	testDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testDir)
+	drive, err := Open(filepath.Join(testDir, "drive.db"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer drive.Close()
+
+	root, version, err := drive.GetRoot()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if root != "" || version != 0 {
+		t.Fatal("unexpected root/version")
+	}
+
+	ok, err := drive.CasRoot("foo", 2, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("unexpected ok")
+	}
+
+	ok, err = drive.CasRoot("foo", 1, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("unexpected ok")
+	}
+
+	ok, err = drive.CasRoot("foo", 2, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("unexpected ok")
+	}
+
+	root, version, err = drive.GetRoot()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if root != "" || version != 0 {
+		t.Fatal("unexpected root/version")
+	}
+
+	ok, err = drive.CasRoot("foo", 1, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Fatal("unexpected !ok")
+	}
+
+	root, version, err = drive.GetRoot()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if root != "foo" || version != 1 {
+		t.Fatal("unexpected root/version")
+	}
+}
